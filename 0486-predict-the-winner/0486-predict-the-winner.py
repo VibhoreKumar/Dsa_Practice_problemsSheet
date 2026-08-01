435042bed45aae39bd47843e4571da9f78e8b73c
Vibhore Kumar <144598771+VibhoreKumar@users.noreply.github.com>
2026-08-01 21:36:40 +0530

    Time: 1643 ms (13.82%), Space: 19.1 MB (94.32%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,14 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def dfs(left, right):
+
+            if left == right:
+                return nums[left]
+
+            takeLeft = nums[left] - dfs(left + 1, right)
+
+            takeRight = nums[right] - dfs(left, right - 1)
+
+            return max(takeLeft, takeRight)
+
+        return dfs(0, len(nums) - 1) >= 0
